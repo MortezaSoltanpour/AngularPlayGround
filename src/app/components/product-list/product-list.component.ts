c2e2299b6af24921ab6715df79a499bfb9f7ed9e
MortezaSoltanpour <morteza.cs@gmail.com>
2025-04-26 10:31:55 -0300

    use ngFor

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,8 +1,9 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
@@ -13,4 +14,23 @@ export class ProductListComponent {
     height: 30,
     wide: 70,
   };
+
+  products = [
+    {
+      name: 'product 1',
+      price: 10000,
+    },
+    {
+      name: 'product 2',
+      price: 20000,
+    },
+    {
+      name: 'product 3',
+      price: 30000,
+    },
+    {
+      name: 'product 4',
+      price: 40000,
+    },
+  ];
 }
