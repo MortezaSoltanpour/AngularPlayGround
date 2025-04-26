1e3819300bd8390e3090171ef92a2d816b9d9556
MortezaSoltanpour <morteza.cs@gmail.com>
2025-04-26 10:28:48 -0300

    use checkbox and ngIf

--- src/app/components/product-create/product-create.component.ts
+++ src/app/components/product-create/product-create.component.ts
@@ -1,17 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
 export class ProductCreateComponent {
   isDisabled: boolean = true;
+  isAgreementChecked: boolean = false;
 
   handleChangeTitle(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
     this.isDisabled = input.value.trim() !== 'morteza';
     console.log(input.value);
+  }
+
+  handleAgreementChanged(e: Event) {
+    const input = e.target as HTMLInputElement;
+    this.isAgreementChecked = input.checked;
   }
 }
