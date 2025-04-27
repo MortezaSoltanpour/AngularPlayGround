1aa5af99887bef7ca112593b2df900fa8530f30a
MortezaSoltanpour <morteza.cs@gmail.com>
2025-04-27 17:18:13 -0300

    Display toast message

--- src/app/components/blog-create/blog-create.component.ts
+++ src/app/components/blog-create/blog-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, NgModule, Output } from '@angular/core';
 import { BlogDto } from '../../db/blogDto';
 import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-blog-create',
@@ -8,15 +9,44 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './blog-create.component.html',
 })
 export class BlogCreateComponent {
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess(message: string) {
+    this.toastr.success(message, 'Success');
+  }
+
+  showError(message: string) {
+    this.toastr.error(message, 'Error');
+  }
+
+  showWarning(message: string) {
+    this.toastr.warning(message, 'Warning');
+  }
+
   blog: BlogDto = this.resetBlogData();
 
   @Output('newblog')
   newBlogEventEmitter: EventEmitter<BlogDto> = new EventEmitter<BlogDto>();
 
+  isEmpty(value: string): boolean {
+    return value.trim() === '';
+  }
+
   handleBtn() {
-    console.log(this.blog);
-    this.newBlogEventEmitter.emit(this.blog);
-    this.resetBlogData();
+    let errorCount = 0;
+    if (this.isEmpty(this.blog.author)) errorCount++;
+    if (this.isEmpty(this.blog.title)) errorCount++;
+    if (this.isEmpty(this.blog.body)) errorCount++;
+    if (this.isEmpty(this.blog.brief)) errorCount++;
+    if (errorCount >= 3) {
+      this.showError('Please fill out all fields');
+    } else if (errorCount >= 1 && errorCount < 3) {
+      this.showWarning('Almost done. but there is still error');
+    } else {
+      this.newBlogEventEmitter.emit(this.blog);
+      this.resetBlogData();
+      this.showSuccess('New post has been added successfully');
+    }
   }
 
   resetBlogData(): BlogDto {
