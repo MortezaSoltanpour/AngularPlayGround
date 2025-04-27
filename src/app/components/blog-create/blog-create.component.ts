360de0ebc6a1eef782120d2dcda110e90f9a9dcc
MortezaSoltanpour <morteza.cs@gmail.com>
2025-04-27 16:33:56 -0300

    Create blog blank page

---
+++ src/app/components/blog-create/blog-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-create',
+  imports: [],
+  templateUrl: './blog-create.component.html',
+})
+export class BlogCreateComponent {}
