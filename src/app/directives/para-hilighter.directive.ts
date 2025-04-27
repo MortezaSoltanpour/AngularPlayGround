45e5a0a463b5c80f1d042c98c3c9a95713864b13
MortezaSoltanpour <morteza.cs@gmail.com>
2025-04-27 18:22:22 -0300

    Get parameters for directive

--- src/app/directives/para-hilighter.directive.ts
+++ src/app/directives/para-hilighter.directive.ts
@@ -1,10 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[paraHilighter]',
 })
-export class ParaHilighterDirective {
-  constructor(element: ElementRef) {
-    element.nativeElement.style.color = 'red';
+export class ParaHilighterDirective implements OnInit {
+  @Input('paraHilighter') color?: string;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const colorToApply =
+      this.color && this.color.trim() !== '' ? this.color : 'red';
+    this.renderer.setStyle(this.el.nativeElement, 'color', colorToApply);
   }
 }
